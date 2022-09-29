f2fc331f6d66aee8508021d8a15306463e0c9122
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-09-29 18:14:36 +1000

    Create navbar component.

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='w-full bg-white shadow-md h-16 flex justify-between items-center mb-10 px-4 sm:px-10'>
+      <p className='text-gradient text-xl xs:text-2xl font-bold '>
+        EvenTracker
+      </p>
+      <div className='flex space-x-2'>
+        <button className='btn-primary'>Login</button>
+        <button className='btn-primary'>Signup</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Pajama from "../assets/images/pajama.jpg";
+
+// <a href="https://www.freepik.com/free-vector/pajamas-party-cartoon-web-banner-invitation_23068299.htm#page=2&query=mobile%20app&position=16&from_view=search">Image by upklyak</a> on Freepik
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <img className='px-10' src={Pajama} width={500} height={800} alt='' />
+    </>
+  );
 };
 
 export default Home;
