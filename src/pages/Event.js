f15f794599ef2e1faf424af9a42b039aa44a057f
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-10-12 12:00:04 +1100

    Add image for event page.

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Celebrate from "../assets/images/celebrate.webp";
 
 const Event = () => {
-  return <div>Event</div>;
+  return (
+    <div>
+      <div>
+        <img className='mb-10 md:p-20' src={Celebrate} alt='celebrate' />
+      </div>
+    </div>
+  );
 };
 
 export default Event;
